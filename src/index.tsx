e0bc380b71778dbda4c2e46db424c5ea818d43be
[dyad] <c.lamattina@gmail.com>
2025-10-25 15:48:35 +0200

    [dyad] wrote 1 file(s)

--- src/index.tsx
+++ src/index.tsx
@@ -37,7 +37,7 @@ const NotificationBell = ({ notifications, onNotificationClick, onMarkAllAsRead,
   return (
     <div className="notification-bell-wrapper">
       <button className="notification-bell" onClick={() => setIsOpen(!isOpen)} aria-label={`Notifiche: ${unreadCount} non lette`}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 0 0 1-3.46 0"></path></svg>
         {unreadCount > 0 && <span className="notification-badge">{unreadCount}</span>}
       </button>
       {isOpen && (
@@ -1796,9 +1796,9 @@ const App = () => {
   const { session } = useSupabase();
 
   return (
-    <>
+    <div className="w-full h-full"> {/* Aggiunto questo div wrapper */}
       {session ? <MainAppContent /> : <AuthForm />}
-    </>
+    </div>
   );
 };
 
